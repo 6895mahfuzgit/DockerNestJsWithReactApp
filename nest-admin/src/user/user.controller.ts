7c5dd14e02ed22dd32e78623a68afcf9bb517300
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-28 21:03:52 +0600

    added AuthGurd to User Controller

--- nest-admin/src/user/user.controller.ts
+++ nest-admin/src/user/user.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Get, Post, UseGuards, UseInterceptors } from '@nestjs/common';
+import { AuthGuard } from '../auth/auth.guard';
 import { UserCreateDto } from '../auth/models/user-create.dto';
 import { User } from './models/user.entity';
 import { UserService } from './user.service';
 
+@UseGuards(AuthGuard)
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('users')
 export class UserController {
 
