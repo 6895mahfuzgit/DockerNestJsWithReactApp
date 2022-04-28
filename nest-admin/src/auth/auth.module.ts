bde0f6e6698d7c31d83c9b2ee3f956d315538fba
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-28 21:21:47 +0600

    use common module to share Jwt Module

--- nest-admin/src/auth/auth.module.ts
+++ nest-admin/src/auth/auth.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { UserModule } from 'src/user/user.module';
+import { CommonModule } from '../common/common.module';
+import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 
 @Module({
   imports:[
    UserModule,
-   JwtModule.register({
-    secret: 'super secret',
-    signOptions: { expiresIn: '1d' },
-  }),
+   CommonModule,
   ],
   controllers: [AuthController]
 })
