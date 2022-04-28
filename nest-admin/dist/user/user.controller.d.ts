01b770b956493217b1ea8b5ada5cba1f0a73a7a9
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-28 20:53:17 +0600

    .

--- nest-admin/dist/user/user.controller.d.ts
+++ nest-admin/dist/user/user.controller.d.ts
@@ -1,6 +1,9 @@
+import { UserCreateDto } from '../auth/models/user-create.dto';
+import { User } from './models/user.entity';
 import { UserService } from './user.service';
 export declare class UserController {
     private userService;
     constructor(userService: UserService);
-    all(): Promise<import("./models/user.entity").User[]>;
+    all(): Promise<User[]>;
+    create(body: UserCreateDto): Promise<User>;
 }
--- nest-admin/dist/user/user.controller.js
+++ nest-admin/dist/user/user.controller.js
@@ -8,9 +8,13 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserController = void 0;
 const common_1 = require("@nestjs/common");
+const user_create_dto_1 = require("../auth/models/user-create.dto");
 const user_service_1 = require("./user.service");
 let UserController = class UserController {
     constructor(userService) {
@@ -19,6 +23,9 @@ let UserController = class UserController {
     async all() {
         return await this.userService.all();
     }
+    async create(body) {
+        return this.userService.create(body);
+    }
 };
 __decorate([
     (0, common_1.Get)(),
@@ -26,6 +33,13 @@ __decorate([
     __metadata("design:paramtypes", []),
     __metadata("design:returntype", Promise)
 ], UserController.prototype, "all", null);
+__decorate([
+    (0, common_1.Post)(),
+    __param(0, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [user_create_dto_1.UserCreateDto]),
+    __metadata("design:returntype", Promise)
+], UserController.prototype, "create", null);
 UserController = __decorate([
     (0, common_1.Controller)('users'),
     __metadata("design:paramtypes", [user_service_1.UserService])
