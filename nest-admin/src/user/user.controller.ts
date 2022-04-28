fc37e49efc6d223309a4d4b2fe0bfff56d5029a7
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-29 02:23:46 +0600

    created user update and delete api in user controller

--- nest-admin/src/user/user.controller.ts
+++ nest-admin/src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Body, ClassSerializerInterceptor, Controller, Get, Param, Post, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Post, Put, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
-import { UserCreateDto } from '../auth/models/user-create.dto';
+import { UserCreateDto } from './models/user-create.dto';
+import { UserUpdateDto } from './models/user-update.dto';
 import { User } from './models/user.entity';
 import { UserService } from './user.service';
 
@@ -24,6 +25,18 @@ export class UserController {
     @Get(':id')
     async get(@Param('id') id :number){
         return this.userService.findOne({id});
+    }
+
+    @Put(':id')
+    async update(@Param('id') id :number,
+                 @Body() body:UserUpdateDto){
+        await this.userService.update(id,body);
+       return this.userService.findOne({id});
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id :number){
+       return this.userService.delete(id);
     }
 
 }
