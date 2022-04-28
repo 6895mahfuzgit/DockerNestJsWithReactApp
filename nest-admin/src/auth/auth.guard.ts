f9419934d69c5776d5cf5ce5351d55e242e79690
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-28 20:38:22 +0600

    Update auth.guard.ts

--- nest-admin/src/auth/auth.guard.ts
+++ nest-admin/src/auth/auth.guard.ts
@@ -1,11 +1,21 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+
+  constructor(private jwtService: JwtService) { }
+
   canActivate(
     context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  ) {
+    const request = context.switchToHttp().getRequest();
+    try {
+      const jwt = request.cookies['jwt'];
+      return this.jwtService.verify(jwt);
+    } catch (e) {
+      return false;
+    }
   }
 }
