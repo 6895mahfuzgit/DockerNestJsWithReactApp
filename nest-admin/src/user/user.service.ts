b1d367961428f3c87d3a0370411b0968abea5e7a
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-30 00:42:52 +0600

    used common repo in userSerive

--- nest-admin/src/user/user.service.ts
+++ nest-admin/src/user/user.service.ts
@@ -1,52 +1,54 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AbstractService } from '../common/abstract/abstract.service';
 import { Repository } from 'typeorm';
 import { User } from './models/user.entity';
 
 @Injectable()
-export class UserService {
+export class UserService  extends AbstractService{
 
     constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {
+       super(userRepository);
     }
 
-    async all(): Promise<User[]> {
-        return this.userRepository.find();
-    }
+    // async all(): Promise<User[]> {
+    //     return this.userRepository.find();
+    // }
    
-    async paginate(page:number=1):Promise<any>{
+    // async paginate(page:number=1):Promise<any>{
 
-       const take=2;
-       const[users,total]=await this.userRepository.findAndCount({
-           take,
-           skip: (page-1)*take
-       });
+    //    const take=2;
+    //    const[users,total]=await this.userRepository.findAndCount({
+    //        take,
+    //        skip: (page-1)*take
+    //    });
 
-       return {
+    //    return {
 
-         data:users,
-         meta:{
-             total,
-             page,
-             last_page:Math.ceil(total/take)
-         }
-       };
+    //      data:users,
+    //      meta:{
+    //          total,
+    //          page,
+    //          last_page:Math.ceil(total/take)
+    //      }
+    //    };
 
-    }
+    // }
 
 
-    async create(data):Promise<User>{
-       return this.userRepository.save(data);
-    }
+    // async create(data):Promise<User>{
+    //    return this.userRepository.save(data);
+    // }
 
-    async findOne(condition):Promise<User>{
-        return this.userRepository.findOne(condition);
-     }
+    // async findOne(condition):Promise<User>{
+    //     return this.userRepository.findOne(condition);
+    //  }
 
-    async update(id:number,data):Promise<any>{
-        return this.userRepository.update(id,data);
-    } 
+    // async update(id:number,data):Promise<any>{
+    //     return this.userRepository.update(id,data);
+    // } 
 
-    async delete(id:number):Promise<any>{
-        return this.userRepository.delete(id);
-    } 
+    // async delete(id:number):Promise<any>{
+    //     return this.userRepository.delete(id);
+    // } 
 }
