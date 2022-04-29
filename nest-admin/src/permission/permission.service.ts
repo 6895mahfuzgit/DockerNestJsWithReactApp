b6509f1b44d488f5c60a51b51faf851c5f91615c
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-29 16:08:26 +0600

    Update permission.service.ts

--- nest-admin/src/permission/permission.service.ts
+++ nest-admin/src/permission/permission.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Permission } from './models/permission.entity';
 
 @Injectable()
-export class PermissionService {}
+export class PermissionService {
+
+    constructor(@InjectRepository(Permission) private readonly permissonRepository:Repository<Permission>){}
+
+    async all():Promise<Permission[]>{
+      return this.permissonRepository.find();
+    }
+
+    async create(data):Promise<Permission>{
+        return this.permissonRepository.save(data);
+     }
+ 
+     async findOne(condition):Promise<Permission>{
+         return this.permissonRepository.findOne(condition);
+      }
+ 
+     async update(id:number,data):Promise<any>{
+         return this.permissonRepository.update(id,data);
+     } 
+ 
+     async delete(id:number):Promise<any>{
+         return this.permissonRepository.delete(id);
+     } 
+}
