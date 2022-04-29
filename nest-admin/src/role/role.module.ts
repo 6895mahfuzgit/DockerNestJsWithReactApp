306c2405b69543706d922cb4fb169eea9fb89a56
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-29 12:36:56 +0600

    Update role.module.ts

--- nest-admin/src/role/role.module.ts
+++ nest-admin/src/role/role.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Role } from './models/role.model';
 import { RoleController } from './role.controller';
 import { RoleService } from './role.service';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([Role]),
+  ],
   controllers: [RoleController],
   providers: [RoleService]
 })
