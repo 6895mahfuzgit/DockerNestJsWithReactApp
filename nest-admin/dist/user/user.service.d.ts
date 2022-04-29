3c21a7604907eb2de98ec99bc2c370d1bed6b50a
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-30 00:41:55 +0600

    used common repo in userSerive

--- nest-admin/dist/user/user.service.d.ts
+++ nest-admin/dist/user/user.service.d.ts
@@ -1,12 +1,7 @@
+import { AbstractService } from '../common/abstract/abstract.service';
 import { Repository } from 'typeorm';
 import { User } from './models/user.entity';
-export declare class UserService {
+export declare class UserService extends AbstractService {
     private readonly userRepository;
     constructor(userRepository: Repository<User>);
-    all(): Promise<User[]>;
-    paginate(page?: number): Promise<any>;
-    create(data: any): Promise<User>;
-    findOne(condition: any): Promise<User>;
-    update(id: number, data: any): Promise<any>;
-    delete(id: number): Promise<any>;
 }
