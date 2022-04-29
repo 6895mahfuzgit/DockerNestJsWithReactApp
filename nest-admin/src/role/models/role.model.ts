6d647c40ca6bf6def8bc399f5e62ea18064d805a
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-29 12:35:52 +0600

    Update role.model.ts

--- nest-admin/src/role/models/role.model.ts
+++ nest-admin/src/role/models/role.model.ts
@@ -1,4 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('roles')
 export class Role{
+    @PrimaryGeneratedColumn()
     id:number;
+
+    @Column()
     name:string;
 }
