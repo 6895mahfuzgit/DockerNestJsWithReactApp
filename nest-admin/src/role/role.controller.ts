3d50150d16e4e54527d924dd4271a8b22c19ac4a
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-29 12:43:41 +0600

    created get all roles api

--- nest-admin/src/role/role.controller.ts
+++ nest-admin/src/role/role.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { RoleService } from './role.service';
 
 @Controller('roles')
-export class RoleController {}
+export class RoleController {
+
+    constructor(private roleService:RoleService){}
+
+    @Get()
+    async all(){
+      return this.roleService.all();  
+    }
+}
