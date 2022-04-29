9d9ed3f2fec534fae4a537cbacd60f8da365d0f9
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-29 16:08:41 +0600

    Update permission.module.ts

--- nest-admin/src/permission/permission.module.ts
+++ nest-admin/src/permission/permission.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PermissionService } from './permission.service';
 import { PermissionController } from './permission.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Permission } from './models/permission.entity';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([Permission]),
+  ],
   providers: [PermissionService],
   controllers: [PermissionController]
 })
