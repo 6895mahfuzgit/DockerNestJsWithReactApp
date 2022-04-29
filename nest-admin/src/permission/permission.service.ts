8bf545eb484f33a8ab26b4038be0040e58049c94
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-30 00:42:14 +0600

    used common repo in permissionSerive

--- nest-admin/src/permission/permission.service.ts
+++ nest-admin/src/permission/permission.service.ts
@@ -1,30 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AbstractService } from '../common/abstract/abstract.service';
 import { Repository } from 'typeorm';
 import { Permission } from './models/permission.entity';
 
 @Injectable()
-export class PermissionService {
+export class PermissionService extends AbstractService {
 
-    constructor(@InjectRepository(Permission) private readonly permissonRepository:Repository<Permission>){}
-
-    async all():Promise<Permission[]>{
-      return this.permissonRepository.find();
+    constructor(@InjectRepository(Permission) private readonly permissonRepository:Repository<Permission>){
+        super(permissonRepository);
     }
 
-    async create(data):Promise<Permission>{
-        return this.permissonRepository.save(data);
-     }
+    // async all():Promise<Permission[]>{
+    //   return this.permissonRepository.find();
+    // }
+
+    // async create(data):Promise<Permission>{
+    //     return this.permissonRepository.save(data);
+    //  }
  
-     async findOne(condition):Promise<Permission>{
-         return this.permissonRepository.findOne(condition);
-      }
+    //  async findOne(condition):Promise<Permission>{
+    //      return this.permissonRepository.findOne(condition);
+    //   }
  
-     async update(id:number,data):Promise<any>{
-         return this.permissonRepository.update(id,data);
-     } 
+    //  async update(id:number,data):Promise<any>{
+    //      return this.permissonRepository.update(id,data);
+    //  } 
  
-     async delete(id:number):Promise<any>{
-         return this.permissonRepository.delete(id);
-     } 
+    //  async delete(id:number):Promise<any>{
+    //      return this.permissonRepository.delete(id);
+    //  } 
 }
