aaf76c934575e762c5a5667a6c1b06534dfcaa37
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-29 12:54:41 +0600

    created role crud API

--- nest-admin/src/role/role.controller.ts
+++ nest-admin/src/role/role.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Role } from './models/role.model';
 import { RoleService } from './role.service';
 
 @Controller('roles')
@@ -9,5 +10,27 @@ export class RoleController {
     @Get()
     async all(){
       return this.roleService.all();  
+    }
+
+    @Post()
+    async create(@Body() name:string):Promise<Role>{
+      return this.roleService.create({name});
+    } 
+
+    @Get(':id')
+    async get(@Param('id') id :number){
+        return this.roleService.findOne({id});
+    }
+
+    @Put(':id')
+    async update(@Param('id') id :number,
+                 @Body() name:string){
+        await this.roleService.update(id,{name});
+       return this.roleService.findOne({id});
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id :number){
+       return this.roleService.delete(id);
     }
 }
