3a1980d8086a6aeb441c803aa2c565644eb4d926
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-30 00:31:34 +0600

    created generic crud service

--- nest-admin/src/common/abstract/abstract.service.ts
+++ nest-admin/src/common/abstract/abstract.service.ts
@@ -1,4 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { PagiratedResult } from '../models/pagirated-result.model';
 
 @Injectable()
-export class AbstractService {}
+export abstract class AbstractService {
+
+    protected constructor(protected repository:Repository<any>){}
+
+    async all(): Promise<any[]> {
+        return this.repository.find();
+    }
+   
+    async paginate(page:number=1):Promise<PagiratedResult>{
+
+       const take=2;
+       const[data,total]=await this.repository.findAndCount({
+           take,
+           skip: (page-1)*take
+       });
+
+       return {
+
+         data:data,
+         meta:{
+             total,
+             page,
+             last_page:Math.ceil(total/take)
+         }
+       };
+
+    }
+
+
+    async create(data):Promise<any>{
+       return this.repository.save(data);
+    }
+
+    async findOne(condition):Promise<any>{
+        return this.repository.findOne(condition);
+     }
+
+    async update(id:number,data):Promise<any>{
+        return this.repository.update(id,data);
+    } 
+
+    async delete(id:number):Promise<any>{
+        return this.repository.delete(id);
+    } 
+
+}
