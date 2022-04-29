90a51e67b1d21f71ba41e75871c1dc405150ce24
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-29 16:08:33 +0600

    Update permission.controller.ts

--- nest-admin/src/permission/permission.controller.ts
+++ nest-admin/src/permission/permission.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Permission } from './models/permission.entity';
+import { PermissionService } from './permission.service';
 
-@Controller('permission')
-export class PermissionController {}
+@Controller('permissions')
+export class PermissionController {
+
+    constructor(private permissionService:PermissionService){}
+
+    @Get()
+    async all(){
+      return this.permissionService.all();  
+    }
+
+    @Post()
+    async create(@Body('name') name:string):Promise<Permission>{
+      return this.permissionService.create({name});
+    } 
+
+    @Get(':id')
+    async get(@Param('id') id :number){
+        return this.permissionService.findOne({id});
+    }
+
+    @Put(':id')
+    async update(@Param('id') id :number,
+                 @Body('name') name:string){
+        await this.permissionService.update(id,{name});
+       return this.permissionService.findOne({id});
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id :number){
+       return this.permissionService.delete(id);
+    }
+}
