0103c8ad8591faad202a7605591190efac0f236e
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-30 16:50:24 +0600

    Update product.module.ts

--- nest-admin/src/product/product.module.ts
+++ nest-admin/src/product/product.module.ts
@@ -4,6 +4,7 @@ import { ProductController } from './product.controller';
 import { Product } from './models/product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from '../common/common.module';
+import { UploadController } from './upload.controller';
 
 @Module({
   imports:[
@@ -11,6 +12,6 @@ import { CommonModule } from '../common/common.module';
     CommonModule
   ],
   providers: [ProductService],
-  controllers: [ProductController]
+  controllers: [ProductController,UploadController]
 })
 export class ProductModule {}
