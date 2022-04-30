5b10de220db2c3d9a38f9d810e45f367c58610df
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-30 16:49:52 +0600

    Create upload.controller.ts

---
+++ nest-admin/src/product/upload.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Param, Post, Req, Res, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express/multer";
+import { diskStorage } from "multer";
+import { AuthGuard } from "../auth/auth.guard";
+import { v4 as uuidv4 } from 'uuid';
+import { Response } from "express";
+
+@UseGuards(AuthGuard)
+@Controller()
+export class UploadController {
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('image', {
+    storage: diskStorage({
+      destination: './uploads',
+      filename: function (req, file, cb) {
+        cb(null, `${uuidv4()}${file.originalname}`)
+      }
+    })
+  }))
+
+  uploadFile(@UploadedFile() file) {
+    console.log(file);
+
+    return {
+      url: `http://localhost:8000/api/${file.path}`
+    }
+
+  }
+
+    @Get('upload/:path')
+    async getImage(@Param('path') path,@Res() res:Response){
+      res.sendFile(path,{root:'./uploads'});
+    }
+
+  
+
+}
