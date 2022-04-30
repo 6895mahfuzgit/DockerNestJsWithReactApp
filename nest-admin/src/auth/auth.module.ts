6ca7f7bbc7c6d2d3f6d678aadd5b6d9e35e6d621
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-04-30 12:47:33 +0600

    used forwardRef to prevent circular dependency in AuthModule

--- nest-admin/src/auth/auth.module.ts
+++ nest-admin/src/auth/auth.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { CommonModule } from '../common/common.module';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
   imports:[
-   UserModule,
+   forwardRef(()=>UserModule),
    CommonModule,
+   AuthModule
   ],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports:[AuthService]
 })
 export class AuthModule {}
