0787995969ef4acb049935c0e36b897f332e37f2
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-05-01 16:00:55 +0600

    Update order.module.ts

--- nest-admin/src/order/order.module.ts
+++ nest-admin/src/order/order.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { Order } from './models/order.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from '../common/common.module';
+import { OrderItem } from './models/order-item.entity';
 
 @Module({
+
+  imports:[
+    TypeOrmModule.forFeature([Order,OrderItem]),
+    CommonModule
+  ],
   providers: [OrderService],
   controllers: [OrderController]
 })
