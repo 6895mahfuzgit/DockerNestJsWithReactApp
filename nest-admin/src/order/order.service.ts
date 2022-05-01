b8a17b2c27c4c6f04eaebb00b5c15b9df2744177
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-05-01 18:00:32 +0600

    Update order.service.ts

--- nest-admin/src/order/order.service.ts
+++ nest-admin/src/order/order.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PagiratedResult } from '../common/models/pagirated-result.model';
 import { Repository } from 'typeorm';
 import { AbstractService } from '../common/abstract/abstract.service';
 import { Order } from './models/order.entity';
@@ -9,6 +10,34 @@ export class OrderService extends AbstractService {
 
     constructor(@InjectRepository(Order) private readonly orderRepository: Repository<Order>) {
         super(orderRepository);
+    }
+
+    async paginate(page: number = 1, relations = []): Promise<PagiratedResult> {
+
+        const take = 2;
+        const [data, total] = await this.repository.findAndCount({
+            take,
+            skip: (page - 1) * take,
+            relations
+        });
+
+        return {
+
+            data: data.map((order: Order) => ({
+                id: order.id,
+                name: order.name,
+                email: order.email,
+                created_at: order.created_at,
+                total: order.total,
+                order_items:order.order_items
+            })),
+            meta: {
+                total,
+                page,
+                last_page: Math.ceil(total / take)
+            }
+        };
+
     }
 
 }
