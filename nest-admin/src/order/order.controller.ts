9da96e3afd36ae922ebba02c71deaa729a3cf9fa
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-05-01 16:04:21 +0600

    Update order.controller.ts

--- nest-admin/src/order/order.controller.ts
+++ nest-admin/src/order/order.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { OrderService } from './order.service';
 
-@Controller('order')
-export class OrderController {}
+@Controller('orders')
+export class OrderController {
+
+
+    constructor(private orderService: OrderService) {
+
+    }
+
+    @Get()
+    async all(@Query('page') page = 1) {
+        return this.orderService.paginate(page, ['order_items']);
+    }
+}
