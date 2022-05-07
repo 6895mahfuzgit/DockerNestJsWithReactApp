7906046b9886a93490eb151cc5ee7af7de3b93f3
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-05-07 12:53:06 +0600

    Update App.tsx

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import Menu from './components/Menu';
+import Users from './pages/Users';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
@@ -13,27 +15,8 @@ function App() {
           <Menu />
 
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-              <h1 className="h2">Dashboard</h1>
-
-            </div>
-
-
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                </tbody>
-              </table>
-            </div>
+            <Dashboard />
+            <Users />
           </main>
         </div>
       </div>
