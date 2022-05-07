b5df6f95cff6328d09a7a8188c9d905cf566d02c
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-05-07 21:25:19 +0600

    assign variable to get form value

--- react-admin/src/pages/Register.tsx
+++ react-admin/src/pages/Register.tsx
@@ -5,16 +5,45 @@ import '../Login.css'
 
 class Register extends Component {
 
+    first_name = ''
+    last_name = ''
+    email = ''
+    password = ''
+    confirm_password = ''
+
+    submit = (e:React.SyntheticEvent) => {
+
+        e.preventDefault();
+        console.log({
+            first_name: this.first_name,
+            last_name: this.last_name,
+            email: this.email,
+            password: this.password,
+            confirm_password: this.confirm_password
+        });
+
+    }
+
     render() {
         return (
             <main className="form-signin">
-                <form>
+                <form onSubmit={this.submit}>
                     <h1 className="h3 mb-3 fw-normal">Please register</h1>
-                    <input className="form-control" placeholder="First Name" required />
-                    <input className="form-control" placeholder="Last Name" required />
-                    <input type="email" className="form-control" placeholder="Email" required />
-                    <input type="password" className="form-control" placeholder="Password" required />
-                    <input type="password" className="form-control" placeholder="Confirm Password" required />
+                    <input className="form-control" placeholder="First Name" required
+                        onChange={e => this.first_name = e.target.value}
+                    />
+                    <input className="form-control" placeholder="Last Name" required
+                        onChange={e => this.last_name = e.target.value}
+                    />
+                    <input type="email" className="form-control" placeholder="Email" required
+                        onChange={e => this.email = e.target.value}
+                    />
+                    <input type="password" className="form-control" placeholder="Password" required
+                        onChange={e => this.password = e.target.value}
+                    />
+                    <input type="password" className="form-control" placeholder="Confirm Password" required
+                        onChange={e => this.confirm_password = e.target.value}
+                    />
                     <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
                 </form>
             </main>
