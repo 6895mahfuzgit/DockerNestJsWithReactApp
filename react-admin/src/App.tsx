d7d6c37563895bf8541f7bcd7c4af19a3932427e
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-05-07 12:13:40 +0600

    created menu component

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -2,25 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
-     <Nav/>
+      <Nav />
       <div className="container-fluid">
         <div className="row">
-          <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-            <div className="position-sticky pt-3">
-              <ul className="nav flex-column">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    <span data-feather="home"></span>
-                    Dashboard
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </nav>
+          <Menu />
 
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
