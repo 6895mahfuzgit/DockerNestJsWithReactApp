ac397ef5134d257e7b1a89cd839d3a5573e4640b
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-05-07 21:04:09 +0600

    added Register template

--- react-admin/src/pages/Register.tsx
+++ react-admin/src/pages/Register.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react';
 import { Component } from 'react';
+import '../Login.css'
 
 
- 
-class  Register extends Component{
+class Register extends Component {
 
-    render() { 
-        return ( 
-            <div>
-                <h1>Register</h1>
-            </div>
-
-         );
+    render() {
+        return (
+            <main className="form-signin">
+                <form>
+                    <h1 className="h3 mb-3 fw-normal">Please register</h1>
+                    <input className="form-control" placeholder="First Name" required />
+                    <input className="form-control" placeholder="Last Name" required />
+                    <input type="email" className="form-control" placeholder="Email" required />
+                    <input type="password" className="form-control" placeholder="Password" required />
+                    <input type="password" className="form-control" placeholder="Confirm Password" required />
+                    <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+                </form>
+            </main>
+        );
     }
 }
- 
+
 export default Register;
