defec1e8a2af34f082770a454690729c3ccb48fe
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-05-08 13:04:57 +0600

    Update App.tsx

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Nav from './components/Nav';
-import Menu from './components/Menu';
 import Users from './pages/Users';
 import Dashboard from './pages/Dashboard';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -17,6 +15,7 @@ function App() {
           <Route path={'/'} element={<Dashboard />} />
           <Route path={'/users'} element={<Users />} />
           <Route path={'/register'} element={<Register />} />
+          <Route path={'/login'} element={<Login />} />
         </Routes>
 
         
