baa42eb7bdc491b761a08de74e5e960376c173ae
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-05-16 21:57:25 +0600

    Added Product and Role route(s).

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -7,6 +7,12 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import UserCreate from './pages/users/UserCreate';
 import UserEdit from './pages/users/UserEdit';
+import Roles from './pages/roles/Roles';
+import RoleCreate from './pages/roles/RoleCreate';
+import RoleEdit from './pages/roles/RoleEdit';
+import Products from './pages/product/Products';
+import ProductCreate from './pages/product/ProductCreate';
+import ProductEdit from './pages/product/ProductEdit';
 
 function App() {
   return (
@@ -17,10 +23,20 @@ function App() {
           <Route path={'/'} element={<Dashboard />} />
           <Route path={'/register'} element={<Register />} />
           <Route path={'/login'} element={<Login />} />
+          
           <Route path={'/users'}  element={<Users />} />
           <Route path={'/users/create'} element={<UserCreate/>} />
           <Route path={'/users/:id/edit'} element={<UserEdit/>} />
 
+          <Route path={'/roles'}  element={<Roles />} />
+          <Route path={'/roles/create'} element={<RoleCreate/>} />
+          <Route path={'/roles/:id/edit'} element={<RoleEdit/>} />
+         
+
+          <Route path={'/products'}  element={<Products/>} />
+          <Route path={'/products/create'} element={<ProductCreate/>} />
+          <Route path={'/products/:id/edit'} element={<ProductEdit/>} />
+         
          
         </Routes>
 
