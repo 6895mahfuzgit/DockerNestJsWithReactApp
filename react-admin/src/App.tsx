75264f03b8c4d35e70e334ca470d75af3e52aeb1
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-05-16 22:28:28 +0600

    Update App.tsx

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Users from './pages/users/Users';
 import Dashboard from './pages/Dashboard';
 import Register from './pages/Register';
 import Login from './pages/Login';
+
+import Users from './pages/users/Users';
 import UserCreate from './pages/users/UserCreate';
 import UserEdit from './pages/users/UserEdit';
+
 import Roles from './pages/roles/Roles';
 import RoleCreate from './pages/roles/RoleCreate';
 import RoleEdit from './pages/roles/RoleEdit';
+
 import Products from './pages/product/Products';
 import ProductCreate from './pages/product/ProductCreate';
 import ProductEdit from './pages/product/ProductEdit';
+
+import Orders from './pages/orders/Orders';
+import OrderEdit from './pages/orders/OrderEdit';
+import OrderCreate from './pages/orders/OrderCreate';
 
 function App() {
   return (
@@ -37,6 +44,12 @@ function App() {
           <Route path={'/products/create'} element={<ProductCreate/>} />
           <Route path={'/products/:id/edit'} element={<ProductEdit/>} />
          
+
+          <Route path={'/orders'}  element={<Orders/>} />
+          <Route path={'/orders/create'} element={<OrderCreate/>} />
+          <Route path={'/orders/:id/edit'} element={<OrderEdit/>} />
+
+          
          
         </Routes>
 
