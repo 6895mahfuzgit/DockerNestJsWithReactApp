a6006ea091be064dd3ae1f6321b13e069be5024a
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-05-16 22:28:54 +0600

    Create order-item.ts

---
+++ react-admin/src/models/order-item.ts
@@ -0,0 +1,7 @@
+export class OrderItems{
+    constructor(public id:number,
+                public product_title:string,
+                public price:number,
+                public quantity:number,
+    ){}
+}
