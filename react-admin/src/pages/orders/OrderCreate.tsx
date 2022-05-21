b1778e67bc54df1ede1c630af674af0c5fa9685f
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2022-05-21 20:55:45 +0600

    Create OrderCreate.tsx

---
+++ react-admin/src/pages/orders/OrderCreate.tsx
@@ -0,0 +1,19 @@
+
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Paginator from '../../components/Paginator';
+import Wrapper from '../../components/Wrapper';
+import { Product } from '../../models/product';
+import { Role } from '../../models/role';
+
+const OrderCreate = () => {
+
+    return (<Wrapper>
+
+        <h1>Orders</h1>
+    </Wrapper>);
+
+}
+
+export default OrderCreate;
